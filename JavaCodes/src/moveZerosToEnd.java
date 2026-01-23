09429ed7a4c82ca7e153d5bf4ac9c18a748d4936
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-01-23 17:17:12 +0530

    move all zero to end

---
+++ JavaCodes/src/moveZerosToEnd.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class moveZerosToEnd {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 0, 4, 0, 5};
+        moveZerosToEnd(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void moveZerosToEnd(int[] arr) {
+        int n = arr.length;
+        int count = 0; // counts non-zero elements
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[count++] = arr[i];
+            }
+        }
+
+        // Fill remaining positions with zeros
+        while (count < n) {
+            arr[count++] = 0;
+        }
+    }
+}
